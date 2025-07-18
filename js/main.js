1cf288d794d8169a7e32b30ecfe2b8866328053b
Thecoteo <tvt742005@gmail.com>
2025-07-18 21:41:53 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,152 @@
+(function ($) {
+	"use strict";
+/*--document ready functions--*/
+    jQuery(document).ready(function($){
+		
+		
+	//typed activate
+
+		/*---- portfolio masonay activation with image load  ------*/
+            var Container =$('.container'); 
+                Container.imagesLoaded( function() {
+                    var festivarMasonry = $('.portolio-masonary').isotope({
+                          layoutMode: 'fitRows',
+                          itemSelector: '.grid-size'
+                        });
+                    $(document).on('click','.porfolio-menu li', function() {
+                      var filterValue = $(this).attr('data-filter');
+                      festivarMasonry.isotope({ filter: filterValue });
+                    });
+            });
+        
+        /*---- portfolio menu active  ------*/
+        var portfolioMenu = '.porfolio-menu li';
+        $(document).on('click',portfolioMenu,function(){
+            $(this).siblings().removeClass('active');
+            $(this).addClass('active');
+        });
+		
+		 /* counter section activation  */
+             var counternumber = $('.counter-number'); 
+                counternumber.counterUp({
+                  delay: 20,
+                  time: 5000
+                });
+		
+		/*--magnific popup Image Activation--*/
+        var imgPopUp =$('.image-popup')
+            imgPopUp.magnificPopup({
+              
+              gallery: {
+                enabled: true
+              },
+              image: {
+                titleSrc: 'title'
+              },
+                type: 'image'
+                
+        });
+		
+		/*--testimonial carousel activate--*/
+        var testimonial = $('#testimonial-slider');
+            testimonial.owlCarousel({
+            loop:true,
+            dots:true,
+            nav:false,
+            center:true,
+            autoplay:true,
+            responsive : {
+              0 : {
+                  items: 1
+              },
+              768 : {
+                  items: 2
+              },
+              960 : {
+                  items: 3
+              },
+              1200 : {
+                  items: 3
+              },
+              1920 : {
+                  items: 3
+              }
+            }
+        }); 
+		/* smoth scroll*/
+		 $('#main-menu li a').on('click', function(event) {
+			 event.preventDefault();
+			 var anchor = $(this).attr('href');
+			  var top = $(anchor).offset().top;
+			     $('html, body').animate({
+						scrollTop: $(anchor).offset().top
+					}, 1000);
+		  });
+        /*bottom to top*/
+        $(document).on('click','.go-top',function(){
+           $("html,body").animate({
+                scrollTop: 0
+            }, 1000);
+        });
+        /*--slick Nav Responsive Navbar activation--*/
+          var SlicMenu = $('#main-menu');
+         SlicMenu.slicknav();
+		/*--- scroll spy activate --*/
+			new ScrollSpy('#main-menu', {
+				nav: '#main-menu > li a',
+				className: 'active'
+			});
+    });
+
+/*--window Scroll functions--*/
+    $(window).on('scroll', function () {
+      /*--show and hide scroll to top --*/
+         var ScrollTop = $('.go-top');
+        if ($(window).scrollTop() > 1000) {
+                ScrollTop.show(1000);
+        } else {
+                ScrollTop.fadeOut(100);
+       }
+        /*--sticky menu activation--*/
+            var mainMenuTop = $('.nav-area');
+            if ($(window).scrollTop() > 300) {
+                mainMenuTop.addClass('nav-fixed');
+            } else {
+                mainMenuTop.removeClass('nav-fixed');
+            }
+        /*--sticky Mobile menu activation--*/
+            var mobileMenuTop = $('.slicknav_menu');
+            if ($(window).scrollTop() > 300) {
+                mobileMenuTop.addClass('nav-fixed');
+            } else {
+                mobileMenuTop.removeClass('nav-fixed');
+            }
+		
+		
+    });
+           
+/*--window load functions--*/
+    $(window).on('load',function(){
+        var preLoder = $(".preloader");
+        preLoder.fadeOut(1000);
+		
+    });
+
+
+    $("#wx").click( function () {
+        if ($("#weixin").is(':hidden')){
+            $("#weixin").slideDown("slow");
+        }else {
+            $("#weixin").slideUp("slow");
+        }
+    });
+    // egg
+    $(".egg").click( function () {
+        $(".footer-area").animate({
+            height: '700px'
+        },3000)
+        $("html,body").animate({
+            scrollTop: 19999
+        }, 3000);
+    });
+}(jQuery));
